39e42059c5346dfa1bbe1905bd644b23df88b354
Artur Apushev <artur.apushev@gmail.com>
2019-04-27 23:22:54 +0300

    Event streams + Aggregates

--- ConsoleRunner/EventStore.fs
+++ ConsoleRunner/EventStore.fs
@@ -2,15 +2,26 @@ module EventStore
 
 type EventProducer<'event> = 'event list -> 'event list
 
+type Aggregate = System.Guid
+
 type EventStore<'event> =
-  { get : unit -> 'event list
-    append : 'event list -> unit
-    evolve : EventProducer<'event> -> unit }
+  { get : unit -> Map<Aggregate, 'event list>
+    getStream : Aggregate -> 'event list
+    append : Aggregate -> 'event list -> unit
+    evolve : Aggregate -> EventProducer<'event> -> unit }
 
 type Msg<'event> =
-  | Append of 'event list
-  | Get of AsyncReplyChannel<'event list>
-  | Evolve of EventProducer<'event>
+  | Append of Aggregate * 'event list
+  | GetStream of Aggregate * AsyncReplyChannel<'event list>
+  | Get of AsyncReplyChannel<Map<Aggregate, 'event list>>
+  | Evolve of Aggregate * EventProducer<'event>
+
+let findEvents aggregate state =
+  state |> Map.tryFind aggregate |> Option.defaultValue []
+
+let addEvents aggregate state events =
+  let existing = state |> findEvents aggregate
+  Map.add aggregate (existing @ events) state
 
 let create() : EventStore<'event> =
   let agent =
@@ -19,19 +30,25 @@ let create() : EventStore<'event> =
         async {
           let! msg = inbox.Receive()
           match msg with
-          | Append events -> return! loop (state @ events)
+          | Append(aggregate, events) -> return! loop (addEvents aggregate state events)
+          | GetStream(aggregate, channel) ->
+            let events = findEvents aggregate state
+            channel.Reply events
+            return! loop state
           | Get channel ->
             channel.Reply state
             return! loop state
-          | Evolve producer ->
-            let newEvents = producer state
-            return! loop (state @ newEvents)
+          | Evolve(aggregate, produceEvents) ->
+            let newState = state |> findEvents aggregate |> produceEvents |> addEvents aggregate state
+            return! loop (newState)
         }
-      loop [])
+      loop Map.empty)
 
-  let append events = agent.Post(Append events)
+  let append aggregate events = agent.Post(Append(aggregate, events))
   let get() = agent.PostAndReply Get
-  let evolve eventProducer = agent.Post(Evolve eventProducer)
+  let evolve aggregate eventProducer = agent.Post(Evolve(aggregate, eventProducer))
+  let getStream aggregate = agent.PostAndReply(fun channel -> GetStream(aggregate, channel))
   { get = get
+    getStream = getStream
     append = append
     evolve = evolve }
--- ConsoleRunner/Program.fs
+++ ConsoleRunner/Program.fs
@@ -8,16 +8,27 @@ open Helpers
 let main _ =
   Tests.run()
   let eventStore : EventStore<Event> = EventStore.create()
-  eventStore.evolve (restockFlavour Vanilla 5)
-  eventStore.evolve (restockFlavour Strawberry -1)
-  eventStore.evolve (sellFlavour Vanilla)
-  eventStore.evolve (sellFlavour Vanilla)
-  eventStore.evolve (sellFlavour Vanilla)
-  eventStore.evolve (sellFlavour Strawberry)
-  let events = eventStore.get()
-  events |> printEvents
-  let sold : Map<Flavour, int> = events |> project soldFlavours
+
+  let truck1 = System.Guid.NewGuid()
+  let truck2 = System.Guid.NewGuid()
+
+  eventStore.evolve truck1 (restockFlavour Vanilla 5)
+  eventStore.evolve truck1 (restockFlavour Strawberry -1)
+  eventStore.evolve truck1 (sellFlavour Vanilla)
+  eventStore.evolve truck1 (sellFlavour Vanilla)
+  eventStore.evolve truck1 (sellFlavour Vanilla)
+  eventStore.evolve truck1 (sellFlavour Strawberry)
+
+  let eventsForTruck1 = eventStore.getStream truck1
+  let eventsForTruck2 = eventStore.getStream truck2
+
+  eventsForTruck1 |> printEvents
+  eventsForTruck2 |> printEvents
+
+  let sold : Map<Flavour, int> = eventsForTruck1 |> project soldFlavours
+
   printSoldFlavour Vanilla sold
   printSoldFlavour Strawberry sold
-  printProjection flavoursInStock events "Flavours In stock"
+
+  printProjection flavoursInStock eventsForTruck1 "Flavours In stock"
   0 // return an integer exit code
